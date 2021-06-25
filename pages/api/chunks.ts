87e9835f780ce346b1d1b4a65ea5577ba319ec94
Rishabh Chawla <rishabh31121999@gmail.com>
2021-06-25 15:06:09 +0530

    feat: add chunks endpoint

---
+++ pages/api/chunks.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { resolve } from 'path';
+import { getAllChunks, clearStore } from '@rishabh3112/pc';
+
+type Data = {
+  directory: string;
+  pages: any;
+};
+
+// TODO: find a way to get commandline args into nextjs application
+const root = resolve(process.cwd(), './');
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    if (req.method === 'POST') {
+        const body = req.body;
+        const tree = await getAllChunks(body.path).then((tree: object) => {
+          clearStore();
+          return tree;
+        });
+        const response = JSON.stringify(
+          {
+            tree,
+            chunks: tree.chunks,
+          },
+          (_, value) => {
+            // Stringify Set
+            if (typeof value === "object" && value instanceof Set) {
+              return [...value];
+            }
+            return value;
+          }
+        );
+        res.json(JSON.parse(response));    
+    }
+}
