7add8a5b50189c24e568151b9a8e73932adc91e1
Rishabh Chawla <rishabh31121999@gmail.com>
2021-06-28 10:47:03 +0530

    chore: remove pagination

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -3,27 +3,16 @@ import { getAllFiles } from '../../utils/getAllFiles';
 
 type Data = {
   directory: string;
-  pages: any;
+  files: string[];
 };
-
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const query: any = req.query.q || '';
   const root = await fetch('http://localhost:3000/args/root').then((res) => res.text());
   const files = await getAllFiles(root, query);
-  const filesPerPage = 10;
-  const pages: any = {};
-
-  files.forEach((file: string, index: number) => {
-    const page: string = Math.floor(index / filesPerPage).toString();
-    if (pages[page] === undefined) {
-      pages[page] = [];
-    }
-    pages[page].push(file);
-  });
 
   res.status(200).json({
     directory: root,
-    pages,
+    files,
   });
 }
