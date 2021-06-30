3642bc9135907ee5b456616fd4979eba7075c0a9
Rishabh Chawla <rishabh31121999@gmail.com>
2021-06-30 16:02:01 +0530

    chore: append collection into collections

--- utils/collections.ts
+++ utils/collections.ts
@@ -1,10 +1,10 @@
 import * as parser from '@babel/parser';
 import traverse from '@babel/traverse';
 import * as t from '@babel/types';
-import { existsSync, readFileSync } from "fs";
+import { existsSync, readFileSync } from 'fs';
 
-
-const createCollection = (name: string, description: string, chunks: string[]) => {
+const createCollection = (collection: { name: string; description: string; chunks: string[] }) => {
+  const { name, description, chunks } = collection;
   const nameProperty = t.objectProperty(t.identifier('name'), t.stringLiteral(name));
   const descriptionProperty = t.objectProperty(t.identifier('description'), t.stringLiteral(description));
   const chunksProperty = t.objectProperty(
@@ -19,35 +19,37 @@ const createCollection = (name: string, description: string, chunks: string[]) =
 };
 
 const addCollection = (configPath: string, collection: { name: string; description: string; chunks: string[] }) => {
-    const sourceCode = readFileSync(configPath).toString();
-    const ast = parser.parse(sourceCode, {
-        sourceType: "module",
-    });
+  const sourceCode = readFileSync(configPath).toString();
+  const ast = parser.parse(sourceCode, {
+    sourceType: 'module',
+  });
+  const collectionNode = createCollection(collection);
 
-    const isCollectionAdded: boolean = false;
-    traverse(ast, {
-        AssignmentExpression(path) {
-            const node = path.node;
-            const isLeftModuleExports: boolean = t.isMemberExpression(node.left) && (node.left as any)?.object?.name === "module" && (node.left as any)?.property?.name === "exports";
-            const isRightObjectExpression: boolean = t.isObjectExpression(node.right);
+  const isCollectionAdded: boolean = false;
+  traverse(ast, {
+    AssignmentExpression(path) {
+      const node = path.node;
+      const isLeftModuleExports: boolean =
+        t.isMemberExpression(node.left) &&
+        (node.left as any)?.object?.name === 'module' &&
+        (node.left as any)?.property?.name === 'exports';
+      const isRightObjectExpression: boolean = t.isObjectExpression(node.right);
 
-            if (!(isLeftModuleExports && isRightObjectExpression)) {
-                return;
-            }
+      if (!(isLeftModuleExports && isRightObjectExpression)) {
+        return;
+      }
 
-            const collectionProperty: any = (node.right as t.ObjectExpression).properties.filter((property: any) => {
-                return property.name === "collections";
-            });
+      const collectionProperty: any = (node.right as t.ObjectExpression).properties.filter((property: any) => {
+        return property.key.name === 'collections';
+      });
 
-            if (collectionProperty.length === 1) {
-                // TODO: append new collection
-            } else {
-                // TODO: create collection property and append collection
-            }
-        }
-    });
+      if (collectionProperty.length === 1) {
+        collectionProperty.value.elements.append(collectionNode);
+      } else {
+        // TODO: create collection property and append collection
+      }
+    },
+  });
 };
 
-export {
-    createCollection
-}
+export { createCollection };
