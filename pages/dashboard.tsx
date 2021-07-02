b0369a50536dd84031cd9dd7ce7e66cfe1bef072
Abhishek Patil <abhiswaspatil1710@gmail.com>
2021-07-02 17:15:26 +0530

    added show chunks from  selected ChunkCollection

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -8,7 +8,7 @@ const useStyles = makeStyles({
   mainContent: {
     display: 'flex',
     flexDirection: 'column',
-    height: '100%',
+    height: '90vh',
   },
   collectionContainer: {
     display: 'flex',
@@ -22,9 +22,8 @@ const useStyles = makeStyles({
   },
   list: {
     width: '100%',
-    maxWidth: 360,
     overflow: 'auto',
-    maxHeight: 300,
+    maxHeight: '400px',
   },
 });
 
@@ -32,7 +31,27 @@ import { useFetch } from '../components/customHooks/useFetch';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { FixedSizeList } from 'react-window';
-import { Checkbox, List } from '@material-ui/core';
+import { Checkbox, Chip, List } from '@material-ui/core';
+import { motion } from 'framer-motion';
+
+const getChunksfromName = (objects: null | { name: string; description: string; chunks: string[] }[], name: string) => {
+  if (!name) {
+    console.log('Choose an item');
+    return <div>Choose an item!</div>;
+  }
+  if (!objects) {
+    console.log('No Collection available yet!');
+    return <div>No Collection available yet!</div>;
+  }
+  const chosenItemObject = objects.find((object) => object.name === name);
+  const chunks = chosenItemObject?.chunks;
+
+  return chunks?.map((chunk: string) => (
+    <motion.div key={chunk} style={{ display: 'inline-block' }} animate={{ scale: 1 }} initial={{ scale: 0.5 }}>
+      <Chip label={chunk} variant="outlined" data-chunk-name={chunk} />
+    </motion.div>
+  ));
+};
 
 function Dashboard() {
   const classes = useStyles();
@@ -48,38 +67,41 @@ function Dashboard() {
     });
   };
 
+  const previewChips = getChunksfromName(dataReceived, chosenItem.name);
+
+  //console.log(dataReceived);
   return (
     <Box className={classes.mainContent} p={5}>
       <Navbar />
       {dataLoading ? null : (
-        <>
-          <Box display="flex" flexDirection="row" justifyContent="space-between" m={1} bgcolor="background.paper">
-            <Box>
-              Hi, list Box
-              <List className={classes.list} component="nav">
-                {!dataReceived ? (
-                  <div>Make Collections To See Them Here</div>
-                ) : (
-                  dataReceived.map((item: { name: string }) => (
-                    <ListItem
-                      button
-                      onClick={() => {
-                        chooseCollection(item.name);
-                      }}
-                      key={item.name}
-                      data-name={item.name}
-                      data-checked={chosenItem.name === item.name ? '1' : '0'}
-                      selected={chosenItem.name === item.name}
-                    >
-                      <ListItemText primary={item.name} />
-                    </ListItem>
-                  ))
-                )}
-              </List>
-            </Box>
-            <Box overflow="auto">Hi, preview Box</Box>
+        <Box display="flex" flexDirection="row" justifyContent="space-evenly" m={1} bgcolor="background.paper">
+          <Box width="40%" border="1px dotted red">
+            Hi, list Box
+            <List className={classes.list} component="nav">
+              {!dataReceived ? (
+                <div>Make Collections To See Them Here</div>
+              ) : (
+                dataReceived.map((item: { name: string }) => (
+                  <ListItem
+                    button
+                    onClick={() => {
+                      chooseCollection(item.name);
+                    }}
+                    key={item.name}
+                    data-name={item.name}
+                    selected={chosenItem.name === item.name}
+                  >
+                    <ListItemText primary={item.name} />
+                  </ListItem>
+                ))
+              )}
+            </List>
           </Box>
-        </>
+          <Box overflow="auto" width="40%" border="1px dotted red">
+            Hi, preview Box
+            <Box>{previewChips}</Box>
+          </Box>
+        </Box>
       )}
     </Box>
   );
