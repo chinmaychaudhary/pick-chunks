e8fc9ad1da60dfd0f9bf1c9e452e3aa4fb9e38ed
Rishabh Chawla <rishabh31121999@gmail.com>
2021-07-19 18:14:34 +0530

    chore: use require

--- utils/collections.ts
+++ utils/collections.ts
@@ -3,6 +3,7 @@ import traverse from '@babel/traverse';
 import generate from '@babel/generator';
 import * as t from '@babel/types';
 import { readFileSync, writeFileSync, existsSync } from 'fs';
+import { relative } from 'path';
 
 /**
  * Creates AST subtree for a chunk collection
@@ -149,53 +150,9 @@ const getCollections = (configPath: string) => {
     return [];
   }
 
-  const sourceCode = readFileSync(configPath).toString();
-  const ast = parser.parse(sourceCode, {
-    sourceType: 'module',
-  });
-  let collections: any[] = [];
-
-  traverse(ast, {
-    AssignmentExpression(path) {
-      const node = path.node;
-      const isLeftModuleExports: boolean =
-        t.isMemberExpression(node.left) &&
-        (node.left as any)?.object?.name === 'module' &&
-        (node.left as any)?.property?.name === 'exports';
-      const isRightObjectExpression: boolean = t.isObjectExpression(node.right);
-
-      if (!(isLeftModuleExports && isRightObjectExpression)) {
-        return;
-      }
-
-      const collectionProperty: any[] = (node.right as t.ObjectExpression).properties.filter((property: any) => {
-        return property.key.name === 'collections';
-      });
-
-      if (collectionProperty.length === 1) {
-        if (!t.isArrayExpression(collectionProperty[0].value)) {
-          throw Error('Collections value should be an array');
-        }
-        collections = collectionProperty[0].value.elements.map((collectionNode: any) => {
-          const collection: any = {};
-          collectionNode.properties.forEach((collectionProperty: any) => {
-            if (t.isStringLiteral(collectionProperty.value)) {
-              collection[collectionProperty.key.name] = collectionProperty.value.value;
-            } else if (t.isArrayExpression(collectionProperty.value)) {
-              collection[collectionProperty.key.name] = collectionProperty.value.elements.map((element: any) => {
-                return element.value;
-              });
-            }
-          });
-          return collection;
-        });
-      } else {
-        collections = [];
-      }
-    },
-  });
-
-  return collections;
+  const relativePath = relative(__dirname, configPath);
+  const configuration = require(relativePath);
+  return configuration.collections || [];
 };
 
 export { addCollection, getCollections };
